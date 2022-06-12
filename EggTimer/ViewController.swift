9b0cb7c9c6fae34b21ec2d38b7173e09449e09c5
arshaansayed <arshaan.h.sayed@gmail.com>
2022-06-12 14:23:39 -0400

    Initial Commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+   
+    @IBAction func eggPress(_ sender: UIButton) {
+        let egg = sender.currentTitle!
+        let times = ["Soft": 5, "Medium": 7, "Hard": 12]
+        var deez : [Float : String] = [5.0:"Deez"]
+        print(deez[5.0]!)
+ 
+        switch egg {
+        case "Soft": print(String(times["Soft"]!) + " You're soft")
+        case "Medium": print(String(times["Medium"]!) + " Boring")
+        case "Hard": print(String(times["Hard"]!) + " Damn bro chill out")
+        default: print("Error")
+        }
+  
+    }
+    
+   
+    
 }
